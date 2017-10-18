7d504df479bc1bae0fb4f1c0a560168a38a13e9f
Laurynas Tamulevicus <laurynas@opinew.com>
2017-10-18 23:45:35 +0100

    added gradient calcualtions for clusters with > 9 tweets

--- event_detection.py
+++ event_detection.py
@@ -1,30 +1,32 @@
 from abc import ABCMeta, abstractmethod
 from operator import attrgetter
-from helper import Helper
+
+import helper
+
 
 class DataFiles:
     CLUSTERS_SORTED_BY_TIME = 'data/coursework/1day/clusters.sortedby.time.csv'
+
 
 class Cluster:
     EPOCH = 5 * 60 * 1000  # 5 mins in ms
     EVENT_PERIOD = 120 * 60 * 1000  # 2 hrs in ms
 
     def __init__(self, clst_id, clst_name, created_time):
-        self.gradients = []
+        self.__gradients = []
         self.id = clst_id
         self.name = clst_name
-        self.tweets = []
-        self.created_time = created_time
+        self.__tweets = []
+        self.created_time = long(created_time)
 
     #
     # def binary_search(self, a, x, lo=0, hi=None):  # can't use a to specify default for hi
     #     hi = hi if hi is not None else len(a)  # hi defaults to len(a)
     #     pos = bisect_left(a, x, lo, hi)  # find insertion position
     #     return (pos if pos != hi and a[pos] == x else -1)  # don't walk off the end
-
     def number_tweets_in_time_period(self, start_time, end_time):
         count = 0
-        for tweet in self.tweets:
+        for tweet in self.__tweets:
             ts = tweet.get_timestamp()
             if ts >= start_time and ts < end_time:
                 count += 1
@@ -33,14 +35,22 @@ class Cluster:
 
         return count
 
-    def get_gradients(self):
-        self.tweets.sort(key=attrgetter('timestamp'))
-        # start_tweet_pos = self.binary_search([tweet.get_timestamp() for tweet in self.tweets], self.created_time)
+    def append(self, tweet):
+        self.__tweets.append(tweet)
 
+    def calc_gradients(self):
+        self.__tweets.sort(key=attrgetter('timestamp'))
+        # start_tweet_pos = self.binary_search([tweet.get_timestamp() for tweet in self.tweets], self.created_time)
         for start in range(self.created_time, self.created_time + Cluster.EVENT_PERIOD, Cluster.EPOCH):
             dy = self.number_tweets_in_time_period(start, start + Cluster.EPOCH)
-            dx = Cluster.EPOCH
-            self.gradients.append(dy * 1.0 / dx)
+            dx = Cluster.EPOCH / 60 / 1000
+            self.__gradients.append(dy * 1.0 / dx)
+
+    def get_gradients(self):
+        return self.__gradients
+
+    def get_tweets(self):
+        return self.__tweets
 
 
 class Entity:
@@ -72,7 +82,7 @@ class Tweet:
         self.id = id
         self.tokens = tokens
         self.clst_id = clst_id
-        self.timestamp = timestamp_ms
+        self.timestamp = long(timestamp_ms)
         self.content = content
         self.user_id = user_id
         self.tokens = []
@@ -83,8 +93,15 @@ class Tweet:
     def get_timestamp(self):
         return self.timestamp
 
+    def __str__(self):
+        return self.id + " " + str(self.timestamp) + " " + self.clst_id + " " + self.content
+
 
 if __name__ == "__main__":
-    helper = Helper()
-    tweets = helper.read_clusters_sorted_by_time(DataFiles.CLUSTERS_SORTED_BY_TIME)
-
+    helper = helper.Helper()
+    tweets, clusters = helper.read_clusters_sorted_by_time(DataFiles.CLUSTERS_SORTED_BY_TIME)
+    print len(clusters), len(tweets)
+    for key in clusters:
+        if len(clusters[key].get_tweets())> 9:
+            clusters[key].calc_gradients()
+            print clusters[key].get_gradients()
--- helper.py
+++ helper.py
@@ -1,16 +1,18 @@
-from event_detection import Tweet, Cluster
+import event_detection as ed
 
 
 class Helper:
-    def read_clusters_sorted_by_time(self, file_name):
+    @staticmethod
+    def read_clusters_sorted_by_time(file_name):
         tweets = []
         clusters = {}
         file_to_read = open(file_name, "r")
         for line in file_to_read.readlines():
             data = line.split(",")
-            tweets.append(
-                Tweet(clst_id=data[0], id=data[2], timestamp_ms=data[3], user_id=data[4], tokens=data[5].split(" "),
-                      content=data[6]))
+            tweet = ed.Tweet(clst_id=data[0], id=data[2], timestamp_ms=data[3], user_id=data[4],
+                             tokens=data[5].split(" "), content=data[6])
+            tweets.append(tweet)
             if not clusters.get(data[0]):
-                clusters[data[0]] = Cluster(clst_id=data[0], clst_name=data[1], created_time=data[3])
+                clusters[data[0]] = ed.Cluster(clst_id=data[0], clst_name=data[1], created_time=data[3])
+            clusters[data[0]].append(tweet)
         return tweets, clusters
