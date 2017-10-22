d6f969e7a9d4ba120ade381e10de2914a27f3f29
Laurynas Tamulevicus <laurynas@opinew.com>
2017-10-22 21:13:16 +0100

    getting events, all filtering components implemented and working

--- event_detection.py
+++ event_detection.py
@@ -1,29 +1,28 @@
+import copy
+import subprocess
 from abc import ABCMeta, abstractmethod
 from operator import attrgetter
-
+import sys
 import helper
 
 
 class DataFiles:
-    CLUSTERS_SORTED_BY_TIME = 'data/coursework/1day/clusters.sortedby.time.csv'
+    CLUSTERS_SORTED_BY_TIME = 'data/coursework/7days/clusters.sortedby.time.csv'
+    NAMED_ENTITIES = 'data/coursework/7days/namedentities.sortedby.time.csv'
 
 
 class Cluster:
-    EPOCH = 5 * 60 * 1000  # 5 mins in ms
-    EVENT_PERIOD = 120 * 60 * 1000  # 2 hrs in ms
-
     def __init__(self, clst_id, clst_name, created_time):
         self.__gradients = []
         self.id = clst_id
         self.name = clst_name
         self.__tweets = []
         self.created_time = long(created_time)
+        self.__entities = []
+        self.entity_names = []
+        self.neigh = []
+        self.entropy = None
 
-    #
-    # def binary_search(self, a, x, lo=0, hi=None):  # can't use a to specify default for hi
-    #     hi = hi if hi is not None else len(a)  # hi defaults to len(a)
-    #     pos = bisect_left(a, x, lo, hi)  # find insertion position
-    #     return (pos if pos != hi and a[pos] == x else -1)  # don't walk off the end
     def number_tweets_in_time_period(self, start_time, end_time):
         count = 0
         for tweet in self.__tweets:
@@ -35,15 +34,20 @@ class Cluster:
 
         return count
 
+    def get_created_time(self):
+        return self.created_time
+
+    def get_id(self):
+        return self.id
+
     def append(self, tweet):
         self.__tweets.append(tweet)
 
     def calc_gradients(self):
         self.__tweets.sort(key=attrgetter('timestamp'))
-        # start_tweet_pos = self.binary_search([tweet.get_timestamp() for tweet in self.tweets], self.created_time)
-        for start in range(self.created_time, self.created_time + Cluster.EVENT_PERIOD, Cluster.EPOCH):
-            dy = self.number_tweets_in_time_period(start, start + Cluster.EPOCH)
-            dx = Cluster.EPOCH / 60 / 1000
+        for start in range(self.created_time, self.created_time + Constants.EVENT_PERIOD, Constants.EPOCH):
+            dy = self.number_tweets_in_time_period(start, start + Constants.EPOCH)
+            dx = Constants.EPOCH / 60 / 1000
             self.__gradients.append(dy * 1.0 / dx)
 
     def get_gradients(self):
@@ -52,12 +56,44 @@ class Cluster:
     def get_tweets(self):
         return self.__tweets
 
+    def get_tweet_ids(self):
+        return [tweet.get_id() for tweet in self.get_tweets()]
+
+    def append_entity(self, entity):
+        self.__entities.append(entity)
+        self.entity_names.append(entity.get_entity_name())
+
+    def get_entities(self):
+        return self.__entities
+
+    def get_entity_names(self):
+        return self.entity_names
+
+    def aggregate_entities(self):
+        for tweet in self.get_tweets():
+            # print tweet
+            for entity in tweet.get_entities():
+                self.append_entity(entity)
+
+    def add_past_neighbour(self, neighbour):
+        self.neigh.append(neighbour)
+
+    def get_past_neighbours(self):
+        return self.neigh
+
+    def get_named_entities(self):
+        named_entities = []
+        for entity in self.get_entities():
+            if isinstance(entity, NamedEntity):
+                named_entities.append(entity)
+        return named_entities
+
 
 class Entity:
     __metaclass__ = ABCMeta
 
     @abstractmethod
-    def get_entity(self):
+    def get_entity_name(self):
         pass
 
 
@@ -65,7 +101,7 @@ class NamedEntity(Entity):
     def __init__(self, named_entity):
         self.entity = named_entity
 
-    def get_entity(self):
+    def get_entity_name(self):
         return self.entity
 
 
@@ -73,7 +109,7 @@ class SimpleEntity(Entity):
     def __init__(self, simple_entity):
         self.entity = simple_entity
 
-    def get_entity(self):
+    def get_entity_name(self):
         return self.entity
 
 
@@ -85,7 +121,7 @@ class Tweet:
         self.timestamp = long(timestamp_ms)
         self.content = content
         self.user_id = user_id
-        self.tokens = []
+        self.tokens = tokens
 
     def add_token(self, token):
         self.tokens.append(token)
@@ -93,15 +129,153 @@ class Tweet:
     def get_timestamp(self):
         return self.timestamp
 
+    def get_id(self):
+        return self.id
+
     def __str__(self):
-        return self.id + " " + str(self.timestamp) + " " + self.clst_id + " " + self.content
+        return self.id + " " + str(self.timestamp) + " " + self.clst_id + " " + self.content + " | " + str(
+            len(self.tokens))
+
+    def get_entities(self):
+        return self.tokens
+
+
+class Constants:
+    MERGING_THR = 1.0
+    MERGING_ITER = 1
+
+    MIN_TWEETS = 14
+
+    MIN_GRADIENT = 0.1
+
+    ENTROPY_THR = 5.0
+
+    NAMED_ENTITIES_THR = 20
+    EPOCH = 5 * 60 * 1000  # 20 mins in ms
+    EVENT_PERIOD = 180 * 60 * 1000  # 2 hrs in ms
+
+    PARAMS = {
+        "trials": 21,
+        "merging_thr": [0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3,  0.2, 0.15, 0.25, 0.1, 0.35, 0.4, 0.45,  0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3],
+        "merging_iter": [1, 1, 1, 1, 1, 1, 1,  1, 1, 1, 1, 1, 1, 1,  1, 1, 1, 1, 1, 1, 1],
+        "min_tweets": [9, 9, 9, 9, 9, 9, 9,  9, 9, 9, 9, 9, 9, 9,  9, 9, 9, 9, 9, 9, 9],
+        "min_gradient": [0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5,  0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5,  0.1, 0.2, 0.3, 0.4, 0.6, 1.0, 2.0],
+        "entropy_thr": [1, 2, 3, 4, 5, 6, 7,  1, 2, 3, 4, 5, 6, 7,  1, 2, 3, 4, 5, 6, 7],
+        "named_entities": [11, 11, 11, 11, 11, 11, 11,  11, 11, 11, 11, 11, 11, 11,  11, 11, 11, 11, 11, 11, 11]
+    }
+
+
+def do_cluster_filtering_tweets_and_named_entities(clusters, min_tweets, min_named_entities):
+    filtered_cl = {}
+    for clst_id in clusters:
+        if len(clusters[clst_id].get_tweets()) >= min_tweets and len(
+                clusters[clst_id].get_named_entities()) >= min_named_entities:
+            filtered_cl[clst_id] = clusters[clst_id]
+    return filtered_cl
+
+
+def do_cluster_merging(clusters, max_iter, merging_thr, helper):
+    merged_clusters = {}
+    for i in range(max_iter):
+        for clst_id in clusters:
+            if not clusters[clst_id]:
+                continue
+            for neigh in clusters[clst_id].get_past_neighbours():
+                if not clusters[clst_id]:
+                    break
+                if not clusters.get(neigh.get_id()):
+                    continue
+
+                to_delete_clst_id = helper.merge_two_clusuters(clusters[clst_id], neigh, merging_thr)
+
+                if to_delete_clst_id:
+                    clusters[to_delete_clst_id] = None
+
+    for clst_id in clusters:
+        if clusters[clst_id]:
+            merged_clusters[clst_id] = clusters[clst_id]
+
+    return merged_clusters
+
+
+def do_entropy_filtering(clusters, min_entropy, helper):
+    result_clusters = {}
+
+    for clst_id in clusters:
+        clusters[clst_id].entropy = helper.calculate_avg_cluster_entropy(clusters[clst_id])
+        if clusters[clst_id].entropy >= min_entropy:
+            result_clusters[clst_id] = clusters[clst_id]
+    return result_clusters
+
+
+def do_gradient_filtering(clusters, min_grad):
+    result_clusters = {}
+    for clst_id in clusters:
+        clusters[clst_id].calc_gradients()
+        if any(grad >= min_grad for grad in clusters[clst_id].get_gradients()):
+            result_clusters[clst_id] = clusters[clst_id]
+    return result_clusters
+
+
+def prepare_clusters_for_writing(clusters):
+    labeled_tweets = {}
+    for clst_id in clusters:
+        for tweet in clusters[clst_id].get_tweets():
+            labeled_tweets[tweet.get_id()] = clst_id
+
+    return labeled_tweets
 
 
 if __name__ == "__main__":
     helper = helper.Helper()
-    tweets, clusters = helper.read_clusters_sorted_by_time(DataFiles.CLUSTERS_SORTED_BY_TIME)
-    print len(clusters), len(tweets)
-    for key in clusters:
-        if len(clusters[key].get_tweets())> 9:
-            clusters[key].calc_gradients()
-            print clusters[key].get_gradients()
+    number_trials = Constants.PARAMS["trials"]
+    sys.setrecursionlimit(10000)
+    init_tweets, init_clusters = helper.read_clusters_sorted_by_time(DataFiles.CLUSTERS_SORTED_BY_TIME,
+                                                                     DataFiles.NAMED_ENTITIES)
+    for i in range(number_trials):
+        print i
+        min_tweets = Constants.PARAMS["min_tweets"][i]
+        named_ent_thr = Constants.PARAMS["named_entities"][i]
+        merging_iter = Constants.PARAMS["merging_iter"][i]
+        merging_thr = Constants.PARAMS["merging_thr"][i]
+        entropy_thr = Constants.PARAMS["entropy_thr"][i]
+        gradient_thr = Constants.PARAMS["min_gradient"][i]
+
+        out_file = "twitter_id.clst_id.over_9_tweets.csv"
+        log = open('log.txt', 'a')
+
+        log.write("\n")
+        log.write("\t\t=========== TRIAL " + str(i) + " ============\n")
+        log.write("PARAMS: \n")
+        log.write("min_tweets:".ljust(20) + str(min_tweets) + "\n")
+        log.write("named_entities:".ljust(20) + str(named_ent_thr) + "\n")
+        log.write("merging_iter:".ljust(20) + str(merging_iter) + "\n")
+        log.write("merging_thr:".ljust(20) + str(merging_thr) + "\n")
+        log.write("entropy_thr:".ljust(20) + str(entropy_thr) + "\n")
+        log.write("min_gradient:".ljust(20) + str(gradient_thr) + "\n")
+        log.write("event_period (min):".ljust(20) + str(Constants.EVENT_PERIOD / 60 / 1000) + "\n")
+        log.write("min_epoch (min):".ljust(20) + str(Constants.EPOCH / 60 / 1000) + "\n")
+        log.close()
+        log = open('log.txt', 'a')
+
+        clusters = copy.deepcopy(init_clusters)
+
+        if not (min_tweets == 0 and named_ent_thr == 0):
+            clusters = do_cluster_filtering_tweets_and_named_entities(clusters, min_tweets, named_ent_thr)
+
+        if merging_thr != -1:
+            clusters = do_cluster_merging(clusters, merging_iter, merging_thr, helper)
+
+        if entropy_thr != 0:
+            clusters = do_entropy_filtering(clusters, entropy_thr, helper)
+
+        if gradient_thr != 0:
+            clusters = do_gradient_filtering(clusters, gradient_thr)
+
+        labeled_tweets = prepare_clusters_for_writing(clusters)
+
+        helper.write_clusters_to_file(out_file, labeled_tweets)
+
+        subprocess.call("python eval.py ../7days/" + out_file, shell=True, cwd="data/coursework/eval/", stdout=log)
+        log.write("==================================================================================\n")
+        log.close()
--- helper.py
+++ helper.py
@@ -1,18 +1,87 @@
+import math
+
 import event_detection as ed
 
 
 class Helper:
     @staticmethod
-    def read_clusters_sorted_by_time(file_name):
+    def read_clusters_sorted_by_time(file_name, named_entities_file_name):
         tweets = []
         clusters = {}
+        clusters_timeline = []
+        named_entities = {}
         file_to_read = open(file_name, "r")
+
+        file_entities = open(named_entities_file_name, 'r')
+
+        for line in file_entities.readlines():
+            data = line.split(",", 1)
+            named_entities[data[0]] = data[1]
+
         for line in file_to_read.readlines():
             data = line.split(",")
+            tokens = [ed.SimpleEntity(entity) if not (entity in named_entities.get(data[2])) else ed.NamedEntity(entity)
+                      for entity in data[5].split(" ")]
             tweet = ed.Tweet(clst_id=data[0], id=data[2], timestamp_ms=data[3], user_id=data[4],
-                             tokens=data[5].split(" "), content=data[6])
+                             tokens=tokens, content=data[6])
             tweets.append(tweet)
             if not clusters.get(data[0]):
-                clusters[data[0]] = ed.Cluster(clst_id=data[0], clst_name=data[1], created_time=data[3])
+                new_cl = ed.Cluster(clst_id=data[0], clst_name=data[1], created_time=data[3])
+                clusters[data[0]] = new_cl
+                oldest_valid_time = new_cl.get_created_time() - ed.Constants.EPOCH*8
+                for past_cluster in clusters_timeline[::-1]:
+                    if past_cluster.get_created_time() >= oldest_valid_time:
+                        new_cl.add_past_neighbour(past_cluster)
+                    else:
+                        break
+                clusters_timeline.append(new_cl)
+
             clusters[data[0]].append(tweet)
+
+        for clst_id in clusters:
+            clusters[clst_id].aggregate_entities()
         return tweets, clusters
+
+    @staticmethod
+    def write_clusters_to_file(file_name, clusters):
+        w_file = open("data/coursework/7days/" + file_name, "w")
+        with w_file:
+            for tweet_id in clusters:
+                w_file.write(str(clusters[tweet_id]) + "," + str(tweet_id) + "\n")
+
+    @staticmethod
+    def merge_two_clusuters(clst_a, clst_b, thr):
+        inter = set(clst_a.get_entity_names()).intersection(clst_b.get_entity_names())
+
+        clst_a_ent = clst_a.get_entities()
+        clst_b_ent = clst_b.get_entities()
+
+        if len(inter) * 1.0 / len(clst_a_ent) >= thr or len(inter) * 1.0 / len(clst_b_ent) >= thr:
+            if len(clst_a_ent) >= len(clst_b_ent):
+                for tweet in clst_b.get_tweets():
+                    tweet.clst_id = clst_a.get_id()
+                    clst_a.append(tweet)
+                for entity in clst_b_ent:
+                    clst_a.append_entity(entity)
+                return clst_b.get_id()
+            else:
+                for tweet in clst_a.get_tweets():
+                    tweet.clst_id = clst_b.get_id()
+                    clst_b.append(tweet)
+                for entity in clst_a_ent:
+                    clst_b.append_entity(entity)
+                return clst_a.get_id()
+        else:
+            return
+
+    @staticmethod
+    def calculate_avg_cluster_entropy(cluster):
+
+        named_entities = [named_ent.get_entity_name() for named_ent in cluster.get_named_entities()]
+
+        entropy = 0
+        N = len(named_entities)
+        for name_entity in named_entities:
+            n_i = named_entities.count(name_entity)
+            entropy += (-1) * (n_i * 1.0 / N) * math.log(n_i * 1.0 / N)
+        return entropy
